14edc13aa5d4d4e759129380df973059aa752b77
Muhib123123 <muhibistanbuly2@gmail.com>
2025-11-08 16:56:27 +0100

    adding text animation to the home page

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,8 +2,45 @@ import pic1 from "../assets/bg-pic.png";
 import "./Home.css";
 import { Link } from "react-router";
 import Header from "./header";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Home() {
+  const [show, setShow] = useState({
+    title: true,
+    p1: false,
+    p2: false,
+    btn: false
+  });
+
+  useEffect(() => {
+    const handleShow = () => {
+      setShow(prevShow => ({
+        ...prevShow,
+        p1: true
+      }));
+    };
+
+    const handleShowP2 = () => {
+      setShow(prevShow => ({
+        ...prevShow,
+        p2: true
+      }));
+    };
+
+    const handleShowBtn = () => {
+      setShow(prevShow => ({
+        ...prevShow,
+        btn: true
+      }));
+    };
+
+    setTimeout(handleShow, 850);
+    setTimeout(handleShowP2, 1700);
+    setTimeout(handleShowBtn, 2550);
+  }, []);
+
+
   return (
     <>
       <title>IY-Home</title>
@@ -13,13 +50,13 @@ function Home() {
 
         <Header />
         <div className="content">
-          <div className="title">
+          <div className={`title ${show.title && "show-home"}`}>
             <h1>IY Sweets</h1>
           </div>
           <div className="text">
-            <p>لمسة بيتية… مذاق لا يُنسى</p>
-            <p className="p2">Fresh Handmade</p>
-            <Link to="/menu">عرض المنتجات</Link>
+            <p className={`p1 ${show.p1 && "show-home"}`}>لمسة بيتية… مذاق لا يُنسى</p>
+            <p className={`p2 ${show.p2 && "show-home"}`}>Fresh Handmade</p>
+            <Link to="/menu" className={`${show.btn && "show-home"}`}>عرض المنتجات</Link>
           </div>
         </div>
       </div>
